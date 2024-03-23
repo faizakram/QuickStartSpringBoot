3916b48363d52242e879c5eb285fbeb34e24a52b
Faiz Akram <156657523+faizorg@users.noreply.github.com>
2024-03-23 23:23:04 +0530

    dynamic history log using aop

--- src/main/java/com/app/controller/HomeController.java
+++ src/main/java/com/app/controller/HomeController.java
@@ -3,18 +3,28 @@ package com.app.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class HomeController {
 
     @GetMapping
-    public ResponseEntity<String> home(){
+    public ResponseEntity<String> home() {
         return new ResponseEntity<>("Running.....", HttpStatus.OK);
     }
 
+    @PostMapping
+    public ResponseEntity<Map<String, Object>> forHistory(@RequestBody Map<String, Object> request) {
+        return new ResponseEntity<>(request, HttpStatus.OK);
+    }
+
+
     @GetMapping("exception")
-    public ResponseEntity<String> exception(){
+    public ResponseEntity<String> exception() {
         throw new RuntimeException("Hello Exception");
     }
 }
