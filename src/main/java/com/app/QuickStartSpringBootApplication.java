f60889337cf53e0c8ad4ed97ff59e90cd2df65a5
Faiz Akram <156657523+faizorg@users.noreply.github.com>
2024-03-23 22:23:31 +0530

    dynamic log using aop

--- src/main/java/com/app/QuickStartSpringBootApplication.java
+++ src/main/java/com/app/QuickStartSpringBootApplication.java
@@ -2,6 +2,8 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
