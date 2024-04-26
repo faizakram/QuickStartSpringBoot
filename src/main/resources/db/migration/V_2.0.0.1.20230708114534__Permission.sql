0e9ca50c30166d1375ead1d3bfe4391fa90025e1
Faiz Akram <156657523+faizorg@users.noreply.github.com>
2024-04-26 23:25:06 +0530

    updated poc

--- src/main/resources/db/migration/V_2.0.0.1.20230708114534__Permission.sql
+++ src/main/resources/db/migration/V_2.0.0.1.20230708114534__Permission.sql
@@ -1,11 +1,13 @@
-CREATE TABLE `permission` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `code` VARCHAR(50) NOT NULL,
-  `name` VARCHAR(50) NOT NULL,
-  `active` BIT(1) NOT NULL,
-  `created_at` DATETIME(6) NOT NULL,
-  `modified_at` DATETIME(6) NOT NULL,
-  `created_by` INT NOT NULL,
-  `modified_by` INT NOT NULL,
-  `group_name` VARCHAR(30) NOT NULL,
-  PRIMARY KEY (`id`));
+CREATE TABLE `permission`
+(
+    `id`          INT         NOT NULL AUTO_INCREMENT,
+    `code`        VARCHAR(50) NOT NULL,
+    `name`        VARCHAR(50) NOT NULL,
+    `active`      BIT(1)      NOT NULL,
+    `created_at`  DATETIME(6) NOT NULL,
+    `modified_at` DATETIME(6) NOT NULL,
+    `created_by`  INT         NOT NULL,
+    `modified_by` INT         NOT NULL,
+    `group_name`  VARCHAR(30) NOT NULL,
+    PRIMARY KEY (`id`)
+);
