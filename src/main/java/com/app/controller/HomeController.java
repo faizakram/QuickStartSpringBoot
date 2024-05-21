34b4afb834ab12e85c27474172788dee9b1a5cd6
Faiz Akram <156657523+faizorg@users.noreply.github.com>
2024-05-21 15:25:53 +0530

    updated poc

--- src/main/java/com/app/controller/HomeController.java
+++ src/main/java/com/app/controller/HomeController.java
@@ -1,15 +1,42 @@
 package com.app.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
+@RequiredArgsConstructor
+@Log4j2
 public class HomeController {
 
-    @GetMapping
-    public ResponseEntity<String> home(){
-        return new ResponseEntity<>("Running.....", HttpStatus.OK);
+    private final ObjectMapper mapper;
+
+    @GetMapping("/")
+    public String home(Model model, @AuthenticationPrincipal OidcUser principal) {
+        if (principal != null) {
+            model.addAttribute("profile", principal.getClaims());
+            return claimsToJson(principal.getClaims());
+
+        }
+        return "No login";
+    }
+
+    private String claimsToJson(Map<String, Object> claims) {
+        try {
+            return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(claims);
+        } catch (JsonProcessingException jpe) {
+            log.error("Error parsing claims to JSON", jpe);
+        }
+        return "Error parsing claims to JSON.";
     }
 }
