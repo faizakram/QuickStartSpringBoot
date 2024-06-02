565ad86b2e84ef1e4a36d5b11675abfed81b4e1f
Faiz Akram <156657523+faizorg@users.noreply.github.com>
2024-06-02 18:39:27 +0530

    updated poc

--- src/main/java/com/app/controller/HomeController.java
+++ src/main/java/com/app/controller/HomeController.java
@@ -2,6 +2,8 @@ package com.app.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping
-    public ResponseEntity<String> home(){
-        return new ResponseEntity<>("Running.....", HttpStatus.OK);
+    public ResponseEntity<String> home(@AuthenticationPrincipal OAuth2User oAuth2User){
+        String name = oAuth2User.getAttribute("name");
+        return new ResponseEntity<>("Running....." +name, HttpStatus.OK);
     }
 }
