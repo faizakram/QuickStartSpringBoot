e866c54a45e1f716a7cbeef8c1067f0e73e7cd0d
IIPL\14261 <faiz.akram@compunneldigitla.com>
2024-09-04 21:33:05 +0530

    Add functionality to upload files to S3 with optional public access Implement file download functionality from S3 as a stream

---
+++ src/main/java/com/app/service/S3Service.java
@@ -0,0 +1,12 @@
+package com.app.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+
+public interface S3Service {
+
+    boolean uploadFile(MultipartFile file, boolean isReadPublicly);
+
+    InputStream downloadFileAsStream(String key);
+}
---
+++ src/main/java/com/app/service/S3ServiceImpl.java
@@ -0,0 +1,82 @@
+package com.app.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.ResponseBytes;
+import software.amazon.awssdk.core.exception.SdkException;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class S3ServiceImpl implements S3Service {
+
+    private final S3Client s3Client;
+
+    @Value("${aws.s3.bucket-name}")
+    private String bucketName;
+
+
+    @Override
+    public boolean uploadFile(MultipartFile file, boolean isReadPublicly) {
+        log.info("Started uploading file '{}' to S3 Bucket '{}'", file.getOriginalFilename(), bucketName);
+        PutObjectRequest putObjectRequest;
+        if (isReadPublicly) {
+            putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(file.getOriginalFilename()).acl("public-read")
+                    .build();
+        } else {
+            putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(file.getOriginalFilename())
+                    .build();
+        }
+        try {
+            s3Client.putObject(putObjectRequest, RequestBody.fromBytes(file.getBytes()));
+            log.info("Successfully uploaded file to S3. Bucket: {}, Key: {}", bucketName, file.getOriginalFilename());
+            return true;
+        } catch (Exception e) {
+            log.error("Failed to upload file to S3. Bucket: {}, Key: {}", bucketName, file.getOriginalFilename(), e);
+            return false;
+        }
+    }
+
+    @Override
+    public InputStream downloadFileAsStream(String key) {
+        try {
+            GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(key)
+                    .build();
+
+            ResponseBytes<GetObjectResponse> getObjectResponse = s3Client.getObjectAsBytes(getObjectRequest);
+            if (getObjectResponse == null) {
+                log.warn("Failed to get file from S3 bucket: Response is null");
+                return new ByteArrayInputStream(new byte[0]);
+            }
+
+            log.info("Successfully getting file in bytes from S3 bucket.");
+            byte[] fileBytes = getObjectResponse.asByteArray();
+            return new ByteArrayInputStream(fileBytes);
+
+        } catch (S3Exception e) {
+            log.error("Failed to fetch object from S3 Bucket: {}, Key: {}", bucketName, key, e);
+            throw e;
+        } catch (SdkException e) {
+            log.error("Error while downloading file from S3 Bucket: {}, Key: {}", bucketName, key, e);
+            throw e;
+        }
+    }
+}
