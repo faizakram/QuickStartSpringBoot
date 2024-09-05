ea2289c09c131c20d13ba8fcef2433d684e2d6cf
IIPL\14261 <faiz.akram@compunneldigitla.com>
2024-09-05 09:59:51 +0530

    add db script

---
+++ src/main/resources/db-scipt/script.sql
@@ -0,0 +1,8 @@
+CREATE TABLE shedlock
+(
+    name       VARCHAR(64) NOT NULL,
+    lock_until TIMESTAMP(3) NULL,
+    locked_at  TIMESTAMP(3) NULL,
+    locked_by  VARCHAR(255) NULL,
+    PRIMARY KEY (name)
+);
